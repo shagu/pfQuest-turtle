ddb7c1e15ae902060e6af9bb7b3ee0190b70cf04
Gurky <90982783+Gurky-Kronos@users.noreply.github.com>
2021-12-20 13:59:26 -0500

    Update units-turtle.lua

--- db/units-turtle.lua
+++ db/units-turtle.lua
@@ -1,7 +1,7 @@
 pfDB["units"]["data-turtle"] = {
   -- remove easter-egg shagu
   [420] = "_",
-    },
+
   -- add turtle-wow shagu
   [60300] = {
     ["fac"] = "H",
@@ -9,6 +9,8 @@ pfDB["units"]["data-turtle"] = {
     ["coords"] = {
       [1] = { 45.0, 59.9, 215, 0 },
     },
+  },
+
   -- add turtle-wow Kheyna Spinpistol
   [81041] = {
     ["fac"] = "H",
@@ -16,4 +18,5 @@ pfDB["units"]["data-turtle"] = {
     ["coords"] = {
       [1] = { 59.3, 44.4, 17, 0 },
     },
+  },
 }
