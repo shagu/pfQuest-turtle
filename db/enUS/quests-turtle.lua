949923458a60b6b4facbeb086bbe8ff51a2ec807
Gurky <90982783+Gurky-Kronos@users.noreply.github.com>
2021-12-21 11:16:52 -0800

    db: goblin starters & race/class bitmasks

--- db/enUS/quests-turtle.lua
+++ db/enUS/quests-turtle.lua
@@ -43,7 +43,7 @@ pfDB["quests"]["enUS-turtle"] = {
     ["T"] = "Down With the Sickness",
     ["O"] = "Collect 10 samples from the Sickly Gazelles wandering the Barrens.",
     ["D"] = "Even as I send you to discover the wondrous source of life here, there is foulness tainting the land. The gentle herds of gazelle are falling ill. We are stymied in our efforts to heal the sick and bring balance to the circle of life. I have sent word home to Thunder Bluff, and through my brethren there, Moonglade. We will have assistance from the Cenarion Circle soon. If you are eager to help, however, I will certainly use you! Go and track the poor beasts that lag behind their herds. You must hunt them before the lionesses bring down the sickly gazelles to feed their prides. Return to me with samples so that I may study what ails our animal friends.",
-  },  
+  },
   [60134] = {
     ["T"] = "Preventing Poison",
     ["O"] = "Vaccinate 10 Gazelle of the Barrens to protect them from harm.",
@@ -63,7 +63,7 @@ pfDB["quests"]["enUS-turtle"] = {
     ["T"] = "Troubles From Distant Lands",
     ["O"] = "Gather 3 types of healing herbs for Aneka Konko in Ratchet.",
     ["D"] = "I offer you humble greetings, denizen of these lands. It is remiss of me to ask this of you, but my people are in grave danger so I cannot withhold from inquiring. My name is Aneka Konko. I hail from Kamio, a land across the seas. Five years ago, we first saw the fish and snake people wash upon our shores. Their encroachments have only continued and our sacred Kami can no longer protect us. I have arrived to this land with a small entourage to seek the help of kind souls who could assist me. Our warriors are fighting to their last breath against the invaders, although, these days most of our warriors die not to the enemy, but to illness. We're ill equipped to treat the many injured. Our island is small and few of us possess the knowledge or reagents needed. When I first came here, I learned of three rare plants that possess immense healing properties. I beg of you to assist us. Retrieve these plants from the three oases in the Barrens. Each oasis contains one of these plants.",
-  },     
+  },
   [70052] = {
     ["T"] = "Trader's Misfortune",
     ["O"] = "Kill Tazan and return to Wharfmaster Dizzywig in Ratchet.",
@@ -74,26 +74,96 @@ pfDB["quests"]["enUS-turtle"] = {
     ["O"] = "Gather 8 Whirling Whizmagigs from Whirling Whizz-Bots, then return to Nert Blastentom.",
     ["D"] = "Alright, pal, listen up. I know this is your first mission for the Venture Company, so I’ll speak real slow. We’re up here to find a special treasure, y'hear? Supposed to be worth more than half the jewels in Undermine all by itself. Boss can’t wait to get his hands on it, but the last guys he sent up here never sent anything back. Thievin’ jerks think they’re gonna keep the boss’s treasure for themselves? Not if I have anything to say about it. Boss says don’t come home unless we get what we came for. We busted up a few gizmos on our flyer gettin’ down here, but luckily for us, it seems like those other guys left some of their bots behind. Run around and grab some whizmagigs from them, would ya?",
   },
+  [80101] = {
+    ["T"] = "Venture Vultures",
+    ["O"] = "Bring Sprat Nozzleton 6 pieces of Plateau Vulture Meat.",
+    ["D"] = "This is the wooooorst! The Venture Co. was supposed to be about fun, adventure, and sweet, sweet profits, but all we’ve done so far is fly up to the middle of nowhere and sit around without any food. Listen, I don’t care what Nert says. I’m not workin’ any more without dinner. You’re going out into the forest, aren't'cha? Then, you’re gonna bring me back some meat from those buzzards. It’s no Booty Bay steak, but you let ol’ Sprat show ya how it’s cooked!",
+  },
+  [80102] = {
+    ["T"] = "Green Versus Green",
+    ["O"] = "Kill 7 Stonetalon Environmentalists and 5 Highpeak Lashers, then return to Nert Blastentom.",
+    ["D"] = "You've gotta be kidding me. Here we are, a mile above every mountain pass, and there’s elves up here? Listen, kid, elves are the most irrational creatures in the world. A good goblin, he looks at a forest and he sees profit! But what does an elf see? Bears? Owls? Little baby squirrels with the big eyes? It makes me sick. Congratulations, you get to be the muscle today. Go out and cut down a few tree-huggers, would ya? And some of their weird living plants while you’re at it. I don’t trust anything that walks around on vines.",
+  },
+  [80103] = {
+    ["T"] = "Risen Oilblazes",
+    ["O"] = "Collect 8 Living Petroleums from Risen Oilblazes in the Scorched Stand, then return to Nert Blastentom.",
+    ["D"] = "Well, I guess we figured out why the elves are up here. The whole dang forest up ahead is on fire! And it’s gotta be goblin-made, too, because no one else uses the same kind of oil! That’s Undermine stuff, kid. There’s not a gnome in the world that can buy that. I don’t know what’s goin’ on, but I don’t like it. Go out and get some of that oil. Maybe it’ll tell us what’s happening, or at least it’ll be a nice prize when we go back if not.",
+  },
   [80104] = {
     ["T"] = "The Other White Mech",
     ["O"] = "Collect 5 Mechanical Drumsticks from the mechanical chickens and return to Sprat Nozzleton.",
     ["D"] = "If there’s one thing I like better than crackin' skulls for profit, it’s cookin’ up brand-new mechanical marvels. There’s a whole butcher’s shop of parts out there in those burnt woods, and buddy, my mouth is waterin’ for a big old mechanical feast! I don’t know why the other expedition would leave so much behind and I don’t care - bring me some of those parts!",
+  },
+  [80105] = {
+    ["T"] = "Ventured Too Far",
+    ["O"] = "Bring the Oil-scorched Note to Nert Blastentom back at the landing site.",
+    ["D"] = "<The note is soaked in oil and scorched by the elemental among whose remains you've found it. It’s difficult to make out most of the writing, but you can decipher one critical fragment.> ...stupid boss didn’t tell us anythin’ about...place. Not a norm-…...tomb. Somethin’ bad buried...-enter of forest... <This seems like i ’s worth reporting back to the expedition.>",
+  },
+  [80106] = {
+    ["T"] = "The Grumbling Grove",
+    ["O"] = "Kill 5 Animated Shredders and 5 Muttering Vultures in the Grumbling Grove.",
+    ["D"] = "Okay, look. Any treasure hunter is gonna find some haunted treasure from time to time, see? And we can’t go back to the boss empty-handed. I mean CAN’T. Like, he’ll have us shipped off to the mines, can’t. So here’s what we’re gonna do, okay? We’re gonna try and get this thing anyway. You’ve done good so far, kid, so you think you’re up for a little more enforcing? There seem to be more of the first expedition’s mechs in the little forest on the other side of the valley, and, uh, some vultures that don’t look real natural. Clear ‘em out, and we’ll see what’s in there.",
+  },
+  [80107] = {
+    ["T"] = "Shadow On The Plateau",
+    ["O"] = "Investigate the tomb in the Grumbling Grove, then return to Nert Blastentom.",
+    ["D"] = "I don’t envy you right now, kid. See, those weird vultures aren’t the worst thing in the woods. Some of the other bruisers were havin’ a look around, and they found a tomb that looks like it's from Uldaman for how old it is. Tried to get close, but they heard some kind of unholy sound from inside. These guys have been around for some of the boss' persuasive interviews before, and they've never heard sounds like that. Said it echoed around the hills all unnatural-like, like it was bouncin' off trees that weren't there. I'll fight elves, I'll fight plant monsters, I'll fight our own blasted tanks, but I am not messing with some ancient cursed tomb. You go out there and find out what's inside so you can kill it, and you and me are friends for good, alright?",
+  },
+  [80108] = {
+    ["T"] = "Me Not Any Kind Of Orc",
+    ["O"] = "Join Nert Blastentom and travel to Durotar to join the Horde.",
+    ["D"] = "There’s only one place in the world that would take in a bunch of smelly refugees with nothing to contribute, kid. The Horde. Yeah, I know they don’t like us. But they don’t like us less than our former associates, get it? We got a chance of winning them over, and there’s no way the Venture Co. is gonna get us if we’re holed up inside of Orgrimmar! It’s time to go straight, little buddy.",
+  },
+  [80115] = {
+    ["T"] = "Grease-stained Letter",
+    ["O"] = "Read the Grease-stained Letter and speak to Grizzie the Enforcer.",
+    ["D"] = "Hey, $N, while you were being the perfect employee doing my--I mean your job, Grizzie the Enforcer was looking for you. Said something about him owing you a favor or whatever. Here, he mentions it in this letter which I may or may have not dropped into my buzzard wings. Don’t take too long, your break is short for a reason!",
+  },
+  [80116] = {
+    ["T"] = "Ooze-covered Letter",
+    ["O"] = "Read the Ooze-covered Letter and speak to Leyti Quicktongue.",
+    ["D"] = "You’re back, $N. Hey, why won’t you tell your friend here, me, the latest gossip of the moment, eh? I hear you owe Leyti Quicktongue some money. Did you gamble all of your savings back home or what? Is that a loan shark? Do I have to pay you health insurance, 'cause I ain't gonna. Now here, read this, but be careful. It has some ugly looking ooze on one of the corners. Short break, and I mean it! Short!",
+  },
+  [80117] = {
+    ["T"] = "Leather-covered Letter",
+    ["O"] = "Read the Leather-covered Letter and speak to Mayten Boomrifle.",
+    ["D"] = "Back for more petty jobs, $N? While that is amazingly boring of you, the local hunter wanted me to give you this. Said something about teaching you to shoot properly! Hmph, that guy think he knows how to teach a goblin to blow and shoot stuff... the audacity! Anyways, take this. Return back quickly or no meal breaks for you today.",
+  },
+  [80118] = {
+    ["T"] = "Fancy Letter",
+    ["O"] = "Read the Fancy Letter and speak to Wizette Icewhistle.",
+    ["D"] = "Good job on those jobs that I sent you to do, $N. As boring as they were, you did your best. Can you conjure me some water? I'm very thirsty! What, you don’t know how to do that yet? Seriously man, here. Read this. Now, go learn something, and don't forget to bring me some sparkling water!",
+  },
+  [80119] = {
+    ["T"] = "Awful-looking Letter",
+    ["O"] = "Read the Awful-looking Letter and speak to Amri Demondeal.",
+    ["D"] = "Pal, $N, back so soon? Eh, we’re pals, right? Why did Amri Demondeal come looking for you? Why won't you tell me? Do you also do the spooky demon stuff like she does? With, like, green fire and all that? Guess I’ll see, won't I? Here, this is for you from her. I hope she never speaks to me again!",
   },
   [80120] = {
     ["T"] = "Garbage Man",
     ["O"] = "Kazznik has asked you to collect the Trash littered throughout the worksite.",
     ["D"] = "Hey, you! Yeah, I'm talkin' to you, pal! I've been picking up our crew's trash all day long and I could really use a break. Say, hows about you clean up for a while? I'll be over here in the shade takin' a quick snooze. When I wake up, I expect this place to be spotless. Now get to work!",
   },
+  [80121] = {
+    ["T"] = "This Is In My Contract",
+    ["O"] = "Collect a Howling Crystal from one of the creatures of the Grumbling Grove.",
+    ["D"] = "Nert might be afraid of a little shadow magic, but not us, huh? It’s time for you learn some real demon-summoning. Yeah, I know we’re in the middle of a crisis, but there’s no time like the present! We’re warlocks! Crises are what we do! And besides, I’m contractually obligated to help you new enforcers out. So, here’s what you’re gonna do. You’re gonna go on Nert’s little expedition, but you’re gonna bring me something special. It looks like some of the creatures there have little shards of a purple crystal on them, and purple just so happens to be our favorite color. Bring me one back, and I’ll teach ya to summon an imp. How’s that?",
+  },
    [80311] = {
     ["T"] = "Mastering the Arcane",
     ["O"] = "Speak to Chok'Garok on a bank of Southfury River in the Barrens",
     ["D"] = "<Ureda glances at you with dismay.> Your skills still require some honing, and your posture betrayes your arrogance. Typical of any new apprentice who bears overconfidence before his own wits. Not that it matters to me. It's your own problem. Any aspiring mage should have his spells ready at all times and think two steps ahead. That's why your lack of awareness should be compensated with at least something you could procure by yourself. There is a… mage who resides on the bank of Southfury River, not far from Thunder Ridge. The task you will recieve from this mage might be arduous at first glance, yet most likely will bear fruit if you put your brain into it.",
-  },     
+  },
    [80312] = {
     ["T"] = "Arcane Arms",
     ["O"] = "Bring Chok'Garok 5 pieces of Moontouched Wood, a Crystal of the Serpent, and an Everchanging Essence from Wailing Caverns.",
     ["D"] = "Ureda sends only worthy mages! Ureda is the worst! <The two heads stare at each other with a surprised look.> We know what you really need as a mage, and we know where to get parts. You bring us parts, and essence too, and we will make you a weapon, yes! Where you get parts? It's in the cave. Barrens! Yes, yes. Wailing Caverns, they call it. Caverns with druids and snakes! Yes. You find us Moontouched Wood, a Crystal of the Serpent, and Everchanging Essence. Yes, the essence! You bring it all to us, and we make it a worthy weapon! Yes, you'll be thankful!",
-  },       
+  },
+  [80388] = {
+    ["T"] = "Stay awhile and listen...",
+    ["O"] = "Complete this quest to become mortal. In this mode you only have one life and can only trade and group up with other mortal players. If you should die, you shall not return again. Customer service will not revive a fallen hero for any reason.",
+    ["D"] = "Hmm, another one? Very well. You wish to prove yourself to your people, to become the strongest, do you not? You wish to become more than just yet another fallen hero, do you not? I have an offer specifically for you. Should you accept it and endure the coming trials, you will become immortal. If you die, you shall not return. I am only giving you this offer here and now. If you change your mind later, there is no coming back.",
+  },
   [80395] = {
     ["T"] = "A Glittering Opportunity",
     ["O"] = "Investigate the mountain south of Ratchet.",
@@ -103,5 +173,5 @@ pfDB["quests"]["enUS-turtle"] = {
     ["T"] = "A Bloody Good Deed",
     ["O"] = "Kill 6 Kolkar Bloodchargers, 4 Kolkar Pack Runners and 4 Kolkar Marauders. Use the crystal to summon Kheyna Spinpistol again.",
     ["D"] = "I am Kheyna Spinpistol. Mechanist, medic, and among other things. I am currently tracking a dangerous foe who threaten the lives of an untold number of innocents. I may ask for your help in dealing with this later… For now, I have a task for you, one that will have quite the generous reward! The nearby Kolkar tribe is preparing for a raid on the Crossroads. I know this because I've used one of my tools, and you could say I sneaked into their tent and heard their Khan ordering it. This can be avoided however. You'll save many lives if you go down there and put an end to them all. They're all around the Oasis to the west. Once you're done come back to me. If I am not here simply touch the crystal again, I will know someone is waiting for me. Now get out there and KILL them all!",
-  },  
+  },
 }
--- db/enUS/units-turtle.lua
+++ db/enUS/units-turtle.lua
@@ -10,15 +10,54 @@ pfDB["units"]["enUS-turtle"] = {
 
   -- add turtle-wow Nert Blastentom starter
   [80100] = "Nert Blastentom",
-  
+
   -- add turtle-wow Sprat Nozzleton
   [80101] = "Sprat Nozzleton",
+
+  -- add turtle-wow Grizzie the Enforcer
+  [80104] = "Grizzie the Enforcer",
+
+  -- add turtle-wow Mayten Boomrifle
+  [80105] = "Mayten Boomrifle",
+
+  -- add turtle-wow Leyti Quicktongue
+  [80106] = "Leyti Quicktongue",
+
+  -- add turtle-wow Amri Demondeal
+  [80107] = "Amri Demondeal",
+
+  -- add turtle-wow Wizette Icewhistle
+  [80108] = "Wizette Icewhistle",
 
   -- add turtle-wow Whirling Whizz-Bot
   [80112] = "Whirling Whizz-Bot",
 
+  -- add turtle-wow Plateau Vulture
+  [80113] = "Plateau Vulture",
+
+  -- add turtle-wow Stonetalon Environmentalist
+  [80114] = "Stonetalon Environmentalist",
+
+  -- add turtle-wow Highpeak Lasher
+  [80115] = "Highpeak Lasher",
+
+  -- add turtle-wow Risen Oilblaze
+  [80116] = "Risen Oilblaze",
+
   -- add turtle-wow Haywire Battlechicken
   [80117] = "Haywire Battlechicken",
+
+  -- add turtle-wow Animated Shredder
+  [80118] = "Animated Shredder",
+
+  -- add turtle-wow Muttering Vulture
+  [80119] = "Muttering Vulture",
+
+  -- add turtle-wow Tomb Shadow
+  [80120] = "Tomb Shadow",
+
+  -- add turtle-wow Nert Blastentom durotar
+  [80121] = "Nert Blastentom",
 
   -- add turtle-wow Kazznik
   [80131] = "Kazznik",
@@ -29,9 +68,12 @@ pfDB["units"]["enUS-turtle"] = {
   -- add turtle-wow Ureda
   [80857] = "Ureda",
 
+  -- add turtle-wow Mysterious Stranger
+  [81030] = "Mysterious Stranger",
+
   -- add turtle-wow Kheyna Spinpistol
   [81041] = "Kheyna Spinpistol",
 
   -- add turtle-wow Aneka Konko
-  [90983] = "Aneka Konko",  
+  [90983] = "Aneka Konko",
 }
