968fa0355a5eaf2f97abefb4b4d432879ead0c59
Gurky <90982783+Gurky-Kronos@users.noreply.github.com>
2021-12-23 02:37:50 -0800

    db: undead area quests
    Quests:
    Cold Feet
    Virulence
    Paint the Roses Red
    Scarlet with Rage
    Don’t Lose (Part Of) Your Head
    Reinforcing The Sepulcher

--- db/enUS/objects-turtle.lua
+++ db/enUS/objects-turtle.lua
@@ -2,6 +2,7 @@ pfDB["objects"]["enUS-turtle"] = {
   [1000370] = "Ripe Garden Pumpkin",
   [1000371] = "Garden Berry Bush",
   [1000372] = "Ripe Garden Watermelon",
+  [1000383] = "Seaweed-covered Footlocker",
   [1772003] = "Summer Dew",
   [1772004] = "Life's Dawn",
   [1772005] = "Vulpa Bloom",
