e8d00e3672afeaf568d3ac25995a6c69c8439c03
shagu <ericmauser@googlemail.com>
2022-02-15 09:56:30 +0100

    db: remove redundant comments

--- db/enUS/units-turtle.lua
+++ db/enUS/units-turtle.lua
@@ -5,450 +5,450 @@ pfDB["units"]["enUS-turtle"] = {
   -- add Vanilla Jhawna Oatwind (new quest in turtle-wow)
   [3884] = "Jhawna Oatwind",
 
-  -- add turtle-wow Godrick Bell
+  -- Godrick Bell
   [50506] = "Godrick Bell",
 
-  -- add turtle-wow Undead Slayer
+  -- Undead Slayer
   [50508] = "Undead Slayer",
 
-  -- add turtle-wow Gina Weller
+  -- Gina Weller
   [51261] = "Gina Weller",
 
-  -- add turtle-wow Throw a coin to the fountain
+  -- Throw a coin to the fountain
   [51301] = "Throw a coin to the fountain",
 
-  -- add turtle-wow Teresa Palin
+  -- Teresa Palin
   [51302] = "Teresa Palin",
 
-  -- add turtle-wow Kern Mosshoof
+  -- Kern Mosshoof
   [51532] = "Kern Mosshoof",
 
-  -- add turtle-wow Snufflesnout
+  -- Snufflesnout
   [51600] = "Snufflesnout",
 
-  -- add turtle-wow shagu
+  -- Shagu
   [60300] = "Shagu",
 
-  -- add turtle-wow Stone Guardian
+  -- Stone Guardian
   [60402] = "Stone Guardian",
 
-  -- add turtle-wow Dietmar Onlooker
+  -- Dietmar Onlooker
   [60404] = "Dietmar Onlooker",
 
-  -- add turtle-wow Zokk Flamefist
+  -- Zokk Flamefist
   [60405] = "Zokk Flamefist",
 
-  -- add turtle-wow Wahjez'fon
+  -- Wahjez'fon
   [60406] = "Wahjez'fon",
 
-  -- add turtle-wow Jovany Staunch
+  -- Jovany Staunch
   [60407] = "Jovany Staunch",
 
-  -- add turtle-wow Logrash
+  -- Logrash
   [60408] = "Logrash",
 
-  -- add turtle-wow Naxiar
+  -- Naxiar
   [60409] = "Naxiar",
 
-  -- add turtle-wow Re’than
+  -- Re’than
   [60410] = "Re’than",
 
-  -- add turtle-wow Ghoststalker
+  -- Ghoststalker
   [60420] = "Ghoststalker",
 
-  -- add turtle-wow Thistlefur Recluse
+  -- Thistlefur Recluse
   [60469] = "Thistlefur Recluse",
 
-  -- add turtle-wow Old Greypaw
+  -- Old Greypaw
   [60470] = "Old Greypaw",
 
-  -- add turtle-wow Faldan Moonshatter
+  -- Faldan Moonshatter
   [60471] = "Faldan Moonshatter",
 
-  -- add turtle-wow Hadanos Greenblade
+  -- Hadanos Greenblade
   [60472] = "Hadanos Greenblade",
 
-  -- add turtle-wow Morpheus Ribcage
+  -- Morpheus Ribcage
   [60483] = "Morpheus Ribcage",
 
-  -- add turtle-wow Liott Maneskin
+  -- Liott Maneskin
   [60484] = "Liott Maneskin",
 
-  -- add turtle-wow Diane Willowfield
+  -- Diane Willowfield
   [60488] = "Diane Willowfield",
 
-  -- add turtle-wow Garfield Sparkblast
+  -- Garfield Sparkblast
   [60496] = "Garfield Sparkblast",
 
-  -- add turtle-wow Shizwackle Tangrig
+  -- Shizwackle Tangrig
   [60500] = "Shizwackle Tangrig",
 
-  -- add turtle-wow Conservationist Yalus
+  -- Conservationist Yalus
   [60507] = "Conservationist Yalus",
 
-  -- add turtle-wow Grubgar
+  -- Grubgar
   [60510] = "Grubgar",
 
-  -- add turtle-wow Boltrus
+  -- Boltrus
   [60513] = "Boltrus",
 
-  -- add turtle-wow Modopo
+  -- Modopo
   [60538] = "Modopo",
 
-  -- add turtle-wow Bristlefur
+  -- Bristlefur
   [60539] = "Bristlefur",
 
-  -- add turtle-wow Taupo Foreststrider
+  -- Taupo Foreststrider
   [70020] = "Taupo Foreststrider",
 
-  -- add turtle-wow Norvok Hawkspear
+  -- Norvok Hawkspear
   [70022] = "Norvok Hawkspear",
 
-  -- add turtle-wow Commander Grushak
+  -- Commander Grushak
   [70023] = "Commander Grushak",
 
-  -- add turtle-wow Ashenvale Stalker
+  -- Ashenvale Stalker
   [70025] = "Ashenvale Stalker",
 
-  -- add turtle-wow Farseer Grimeye
+  -- Farseer Grimeye
   [70027] = "Farseer Grimeye",
 
-  -- add turtle-wow Discover what's hiding in the Barrow Den
+  -- Discover what's hiding in the Barrow Den
   [70028] = "Discover what's hiding in the Barrow Den",
 
-  -- add turtle-wow Nert Blastentom starter
+  -- Nert Blastentom starter
   [80100] = "Nert Blastentom",
 
-  -- add turtle-wow Sprat Nozzleton
+  -- Sprat Nozzleton
   [80101] = "Sprat Nozzleton",
 
-  -- add turtle-wow Grizzie the Enforcer
+  -- Grizzie the Enforcer
   [80104] = "Grizzie the Enforcer",
 
-  -- add turtle-wow Mayten Boomrifle
+  -- Mayten Boomrifle
   [80105] = "Mayten Boomrifle",
 
-  -- add turtle-wow Leyti Quicktongue
+  -- Leyti Quicktongue
   [80106] = "Leyti Quicktongue",
 
-  -- add turtle-wow Amri Demondeal
+  -- Amri Demondeal
   [80107] = "Amri Demondeal",
 
-  -- add turtle-wow Wizette Icewhistle
+  -- Wizette Icewhistle
   [80108] = "Wizette Icewhistle",
 
-  -- add turtle-wow Whirling Whizz-Bot
+  -- Whirling Whizz-Bot
   [80112] = "Whirling Whizz-Bot",
 
-  -- add turtle-wow Plateau Vulture
+  -- Plateau Vulture
   [80113] = "Plateau Vulture",
 
-  -- add turtle-wow Stonetalon Environmentalist
+  -- Stonetalon Environmentalist
   [80114] = "Stonetalon Environmentalist",
 
-  -- add turtle-wow Highpeak Lasher
+  -- Highpeak Lasher
   [80115] = "Highpeak Lasher",
 
-  -- add turtle-wow Risen Oilblaze
+  -- Risen Oilblaze
   [80116] = "Risen Oilblaze",
 
-  -- add turtle-wow Haywire Battlechicken
+  -- Haywire Battlechicken
   [80117] = "Haywire Battlechicken",
 
-  -- add turtle-wow Animated Shredder
+  -- Animated Shredder
   [80118] = "Animated Shredder",
 
-  -- add turtle-wow Muttering Vulture
+  -- Muttering Vulture
   [80119] = "Muttering Vulture",
 
-  -- add turtle-wow Tomb Shadow
+  -- Tomb Shadow
   [80120] = "Tomb Shadow",
 
-  -- add turtle-wow Nert Blastentom durotar
+  -- Nert Blastentom durotar
   [80121] = "Nert Blastentom",
 
-  -- add turtle-wow Kazznik
+  -- Kazznik
   [80131] = "Kazznik",
 
-  -- add turtle-wow Grand Taskmaster Nert Blastentom
+  -- Grand Taskmaster Nert Blastentom
   [80178] = "Grand Taskmaster Nert Blastentom",
 
-  -- add turtle-wow Tirisfal Plagued Bear
+  -- Tirisfal Plagued Bear
   [80466] = "Tirisfal Plagued Bear",
 
-  -- add turtle-wow Chok'Garok
+  -- Chok'Garok
   [80831] = "Chok'Garok",
 
-  -- add turtle-wow Ureda
+  -- Ureda
   [80857] = "Ureda",
 
-  -- add turtle-wow Knobby Tinfault
+  -- Knobby Tinfault
   [80901] = "Knobby Tinfault",
 
-  -- add turtle-wow Miley Wildbit
+  -- Miley Wildbit
   [80902] = "Miley Wildbit",
 
-  -- add turtle-wow Ostarius
+  -- Ostarius
   [80935] = "Ostarius",
 
-  -- add turtle-wow Mysterious Stranger
+  -- Mysterious Stranger
   [81030] = "Mysterious Stranger",
 
-  -- add turtle-wow Kheyna Spinpistol
+  -- Kheyna Spinpistol
   [81041] = "Kheyna Spinpistol",
 
-  -- add turtle-wow Dark Ranger Lanissa
+  -- Dark Ranger Lanissa
   [81050] = "Dark Ranger Lanissa",
 
-  -- add turtle-wow Aneka Konko
+  -- Aneka Konko
   [90983] = "Aneka Konko",
 
-  -- add turtle-wow Makura Thresher
+  -- Makura Thresher
   [91193] = "Makura Thresher",
 
-  -- add turtle-wow Makura Oilclaw
+  -- Makura Oilclaw
   [91194] = "Makura Oilclaw",
 
-  -- add turtle-wow Wrix Ozzlenut
+  -- Wrix Ozzlenut
   [91200] = "Wrix Ozzlenut",
 
-  -- add turtle-wow Rugnar
+  -- Rugnar
   [91203] = "Rugnar",
 
-  -- add turtle-wow Oilworker Razlik
+  -- Oilworker Razlik
   [91208] = "Oilworker Razlik",
 
-  --add turtle-wow Technician Spuzzle
+  --Technician Spuzzle
   [91214] = "Technician Spuzzle",
 
-  -- add turtle-wow Hargosh
+  -- Hargosh
   [91227] = "Hargosh",
 
-  -- add turtle-wow Technician Grimzlow
+  -- Technician Grimzlow
   [91234] = "Technician Grimzlow",
 
-  -- add turtle-wow Tradesman Laz
+  -- Tradesman Laz
   [91237] = "Tradesman Laz",
 
-  -- add turtle-wow Gowlfang
+  -- Gowlfang
   [91243] = "Gowlfang",
 
-  -- add turtle-wow Pazzle Brightwrench
+  -- Pazzle Brightwrench
   [91259] = "Pazzle Brightwrench",
 
-  -- add turtle-wow Shak 'The Hack'
+  -- Shak 'The Hack'
   [91260] = "Shak 'The Hack'",
 
-  -- add turtle-wow Miner Axel
+  -- Miner Axel
   [91272] = "Miner Axel",
 
-  -- add turtle-wow Big Fraggle
+  -- Big Fraggle
   [91274] = "Big Fraggle",
 
-  -- add turtle-wow Grimm Onearm
+  -- Grimm Onearm
   [91275] = "Grimm Onearm",
 
-  -- add turtle-wow Brizclang Spuckettool
+  -- Brizclang Spuckettool
   [91280] = "Brizclang Spuckettool",
 
-  -- add turtle-wow Grol the Exile
+  -- Grol the Exile
   [91285] = "Grol the Exile",
 
-  -- add turtle-wow Daela Evermoon
+  -- Daela Evermoon
   [91287] = "Daela Evermoon",
 
-  -- add turtle-wow Ardaen Evermoon
+  -- Ardaen Evermoon
   [91288] = "Ardaen Evermoon",
 
-  -- add turtle-wow Lord Rog
+  -- Lord Rog
   [91289] = "Lord Rog",
 
-  -- add turtle-wow Chief Yin'do
+  -- Chief Yin'do
   [91290] = "Chief Yin'do",
 
-  -- add turtle-wow Sagepaw
+  -- Sagepaw
   [91291] = "Sagepaw",
 
-  -- add turtle-wow Tarlo Farcrack
+  -- Tarlo Farcrack
   [91292] = "Tarlo Farcrack",
 
-  -- add turtle-wow Archivist Landas
+  -- Archivist Landas
   [91293] = "Archivist Landas",
 
-  -- add turtle-wow Shazknock
+  -- Shazknock
   [91298] = "Shazknock",
 
-  -- add turtle-wow Foreman Klix
+  -- Foreman Klix
   [91302] = "Foreman Klix",
 
-  -- add turtle-wow Remnants Footman
+  -- Remnants Footman
   [91352] = "Remnants Footman",
 
-  -- add turtle-wow Raz'Dag
+  -- Raz'Dag
   [91400] = "Raz'Dag",
 
-  -- add turtle-wow Daffar
+  -- Daffar
   [91401] = "Daffar",
 
-  -- add turtle-wow Jinz'ah
+  -- Jinz'ah
   [91402] = "Jinz'ah ",
 
-  -- add turtle-wow Speaker Ganz'ih
+  -- Speaker Ganz'ih
   [91411] = "Speaker Ganz'ih",
 
-  -- add turtle-wow Gortog
+  -- Gortog
   [91412] = "Gortog",
 
-  -- add turtle-wow Titiki
+  -- Titiki
   [91415] = "Titiki",
 
-  -- add turtle-wow Duchess Grelda
+  -- Duchess Grelda
   [91711] = "Duchess Grelda",
 
-  -- add turtle-wow Duke Nargelas
+  -- Duke Nargelas
   [91712] = "Duke Nargelas",
 
-  -- add turtle-wow Apothecary Volgrin
+  -- Apothecary Volgrin
   [91713] = "Apothecary Volgrin",
 
-  -- add turtle-wow Pack leader Sharn
+  -- Pack leader Sharn
   [91715] = "Pack Leader Sharn",
 
-  -- add turtle-wow Chief Rnarl
+  -- Chief Rnarl
   [91716] = "Chief Rnarl",
 
-  -- add turtle-wow Belgar
+  -- Belgar
   [91717] = "Belgar",
 
-  -- add turtle-wow Storn
+  -- Storn
   [91718] = "Storn",
 
-  -- add turtle-wow Duchess Faliona
+  -- Duchess Faliona
   [91721] = "Duchess Faliona",
 
-  -- add turtle-wow Father Brightcopf
+  -- Father Brightcopf
   [91726] = "Father Brightcopf",
 
-  -- add turtle-wow Butcher Reeves
+  -- Butcher Reeves
   [91727] = "Butcher Reeves",
 
-  -- add turtle-wow Marlow Neggle
+  -- Marlow Neggle
   [91728] = "Marlow Neggle",
 
-  -- add turtle-wow Pesterhide Forager
+  -- Pesterhide Forager
   [91735] = "Pesterhide Forager",
 
-  -- add turtle-wow Pesterhide Gnoll
+  -- Pesterhide Gnoll
   [91736] = "Pesterhide Gnoll",
 
-  -- add turtle-wow Pesterhide Warrior
+  -- Pesterhide Warrior
   [91737] = "Pesterhide Warrior",
 
-  -- add turtle-wow Pesterhide Mystic
+  -- Pesterhide Mystic
   [91738] = "Pesterhide Mystic",
 
-  -- add turtle-wow Tirisclaw Scavenger
+  -- Tirisclaw Scavenger
   [91739] = "Tirisclaw Scavenger",
 
-  -- add turtle-wow Tirisclaw Ravager
+  -- Tirisclaw Ravager
   [91740] = "Tirisclaw Ravager",
 
-  -- add turtle-wow Tirisclaw Shadowcaster
+  -- Tirisclaw Shadowcaster
   [91741] = "Tirisclaw Shadowcaster",
 
-  -- add turtle-wow Bounty Hunter Knox
+  -- Bounty Hunter Knox
   [91749] = "Bounty Hunter Knox",
 
-  -- add turtle-wow Deathguard Mike
+  -- Deathguard Mike
   [91752] = "Deathguard Mike",
 
-  -- add turtle-wow Vathras Millson
+  -- Vathras Millson
   [91753] = "Vathras Millson",
 
-  -- add turtle-wow Wretched Farmhand
+  -- Wretched Farmhand
   [91756] = "Wretched Farmhand",
 
-  -- add turtle-wow Wretched Guard
+  -- Wretched Guard
   [91757] = "Wretched Guard",
 
-  -- add turtle-wow Necromancer Reginald
+  -- Necromancer Reginald
   [91759] = "Necromancer Reginald",
 
-  -- add turtle-wow Tirisclaw Ambusher
+  -- Tirisclaw Ambusher
   [91760] = "Tirisclaw Ambusher",
 
-  -- add turtle-wow Remnants Sentry
+  -- Remnants Sentry
   [91772] = "Remnants Sentry",
 
-  -- add turtle-wow Remnants Scout
+  -- Remnants Scout
   [91773] = "Remnants Scout",
 
-  -- add turtle-wow Witherbark Raider
+  -- Witherbark Raider
   [91784] = "Witherbark Raider",
 
-  -- add turtle-wow Witherbark Rogue
+  -- Witherbark Rogue
   [91785] = "Witherbark Rogue",
 
-  -- add turtle-wow Witherbark Soothsayer
+  -- Witherbark Soothsayer
   [91786] = "Witherbark Soothsayer",
 
-  -- add turtle-wow Warleader Kintoza
+  -- Warleader Kintoza
   [91789] = "Warleader Kintoza",
 
-  -- add turtle-wow Speaker Gan'to
+  -- Speaker Gan'to
   [91790] = "Speaker Gan'to",
 
-  -- add turtle-wow Heketh
+  -- Heketh
   [91791] = "Heketh",
 
-  -- add turtle-wow Captain Silas
+  -- Captain Silas
   [91974] = "Captain Silas",
 
-  -- add turtle-wow Remnants Priest
+  -- Remnants Priest
   [91975] = "Remnants Priest",
 
-  -- add turtle-wow Barkeep Clemens
+  -- Barkeep Clemens
   [91976] = "Barkeep Clemens",
 
-  -- add turtle-wow Remnants Farmer
+  -- Remnants Farmer
   [91980] = "Remnants Farmer",
 
-  -- add turtle-wow Remnants Militia
+  -- Remnants Militia
   [91981] = "Remnants Militia",
 
-  -- add turtle-wow Cragtusk Boar
+  -- Cragtusk Boar
   [91987] = "Cragtusk Boar",
 
-  -- add turtle-wow Graypaw Wolf
+  -- Graypaw Wolf
   [91989] = "Graypaw Wolf",
 
-  -- add turtle-wow Groveweald Warrior
+  -- Groveweald Warrior
   [92100] = "Groveweald Warrior",
 
-  -- add turtle-wow Groveweald Shaman
+  -- Groveweald Shaman
   [92101] = "Groveweald Shaman",
 
-  -- add turtle-wow Groveweald Pathfinder
+  -- Groveweald Pathfinder
   [92102] = "Groveweald Pathfinder",
 
-  -- add turtle-wow Groveweald Warder
+  -- Groveweald Warder
   [92103] = "Groveweald Warder",
 
-  -- add turtle-wow Groveweald Ursa
+  -- Groveweald Ursa
   [92104] = "Groveweald Ursa",
 
-  -- add turtle-wow Elder 'One Eye'
+  -- Elder 'One Eye'
   [92105] = "Elder 'One Eye'",
 
-  -- add turtle-wow Elder Blackmaw
+  -- Elder Blackmaw
   [92106] = "Elder Blackmaw",
 
-  -- add turtle-wow Grovetender Engryss
+  -- Grovetender Engryss
   [92107] = "Grovetender Engryss",
 
-  -- add turtle-wow Graypaw Wolf
+  -- Graypaw Wolf
   [91989] = "Graypaw Wolf",
 
-  -- add turtle-wow Master Raxxieth
+  -- Master Raxxieth
   [92110] = "Master Raxxieth",
 }
