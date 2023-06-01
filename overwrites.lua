a8acb1113ec46b9a860357e68495348e85ca86fd
zeress <alfred.frendocumbo@gmail.com>
2023-06-01 14:54:23 -0300

    Revert "Moved Angus Stern's entry"
    This reverts commit 39c130edaf1ca417f393c9100bc4a2007dbe3dae.

--- overwrites.lua
+++ overwrites.lua
@@ -3,6 +3,15 @@
 -- To be able to customize and/or fix entries that aren't yet detected
 -- by the extractor logic, this file here can be used to save overwrites.
 
+-- Angus Stern (Stormwind)
+pfDB["units"]["data-turtle"][1141] = {
+  ["coords"] = {
+    [1] = { 51.8, 93.6, 1519, 30 },
+  },
+  ["fac"] = "A",
+  ["lvl"] = "60",
+}
+
 -- Highlord Bolvar Fordragon (Stormwind)
 pfDB["units"]["data-turtle"][1748] = {
   ["coords"] = {
@@ -19,15 +28,6 @@ pfDB["units"]["data-turtle"][1749] = {
   },
   ["fac"] = "A",
   ["lvl"] = "62",
-}
-
--- Angus Stern (Stormwind)
-pfDB["units"]["data-turtle"][1141] = {
-  ["coords"] = {
-    [1] = { 51.8, 93.6, 1519, 30 },
-  },
-  ["fac"] = "A",
-  ["lvl"] = "60",
 }
 
 -- Sorrowguard Keep NPCs
